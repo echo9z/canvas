bf2d7eab0c6ea4aec15ec5a937b392a6a8d9948b
echo9z <1131223522@qq.com>
2023-09-30 23:14:03 +0800

    docs: learn pixi

---
+++ pixi/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: "localhost",
+    https: false, //是否启用 http 2
+    cors: true, //为开发服务器配置 CORS , 默认启用并允许任何源
+    open: true, //服务启动时自动在浏览器中打开应用
+    port: 9000,
+  }
+})
